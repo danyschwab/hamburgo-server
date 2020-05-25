56d1e07765fd55b0c5a51f4df089d53081e16cdf
Daniela Schwab <daniela.schwab@guiabolso.com.br>
2020-05-25 19:26:58 -0300

    Adding env variables

--- start.js
+++ start.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const data = require('./data.js')
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 const app = express();
 app.use(express.json());
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = process.env.HOST;
+const port = process.env.PORT;
 
 app.get('/api/v1/', (req, res) => {
 	var aux = data.getAll()
 	res.send(aux);
 });
 
-app.listen(3000);
-console.log("Listen on port 3000");
-
+app.listen(port, hostname, function() {console.log(`App listening on port ${port}!`)})
